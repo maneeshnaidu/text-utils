416d6508eadb58b96385e5009e47b09e2f0a5738
maneeshnaidu <naidum69@gmail.com>
2023-05-13 22:52:38 +1200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CustomNav from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <CustomNav /> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
